048c90d2788846e8d6ea50ce423d22b016d9f9e9
Nooble <kieranaslett@gmail.com>
2017-06-22 11:17:37 +0100

    update

--- requirements.bat
+++ requirements.bat
@@ -1,2 +1,2 @@
-py -m pip install PyTMX-3.20.18.tar.gz
-py -m pip install pygame-1.9.3-cp36-cp36m-win_amd64.whl
+py -m pip install lib/PyTMX-3.20.18.tar.gz
+py -m pip install lib/pygame-1.9.3-cp36-cp36m-win_amd64.whl
